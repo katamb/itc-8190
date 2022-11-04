976185a6d66f26e7c7598d9517ae0c4d06636aa6
KarlTamberg <tamberg.karl@gmail.com>
2022-11-04 20:26:12 +0200

    Implement steps 3 and 4;

---
+++ src/main/java/itc8190/AKSSlow.java
@@ -0,0 +1,102 @@
+package itc8190;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Sources used for help/inspiration:
+ * https://www.youtube.com/watch?v=HvMSRWTE2mI&ab_channel=Numberphile
+ * https://en.wikipedia.org/wiki/AKS_primality_test
+ * https://www.geeksforgeeks.org/aks-primality-test/
+ * https://en.wikipedia.org/wiki/Pascal%27s_triangle
+ * https://www.geeksforgeeks.org/pascal-triangle/
+ */
+public class AKSSlow {
+
+    /**
+     * Using Pascal triangle, find coefficients for (x-1)^n.
+     * I believe in this use case the sign (+/-) of the coefficients does not play any role, however adding the correct
+     * signs does not change the complexity in O notation, so adding them seemed reasonable in this case.
+     * The big O complexity of this method is O(n^2/2), which for large inputs can be noted as O(n^2)
+     *
+     * @param testValue - integer we are testing to be prime or not
+     * @return list of coefficients
+     */
+    static List<BigInteger> findCoefficients(Integer testValue) {
+        List<BigInteger> previousRow = new ArrayList<>();
+        List<BigInteger> currentRow = new ArrayList<>();
+
+        for (int row = 1; row <= testValue; row++) {
+            boolean lastRow = row == testValue;
+
+            currentRow = new ArrayList<>();
+            for (int column = 0; column <= row; column++) {
+                if (column == 0) {
+                    currentRow.add(BigInteger.ONE);
+                } else if (row == column) {
+                    // Adding the correct sign to last value of the current row
+                    currentRow.add(lastRow && column % 2 == 1 ? BigInteger.ONE.negate() : BigInteger.ONE);
+                } else {
+                    BigInteger rowValue = previousRow.get(column - 1).add(previousRow.get(column));
+                    if (lastRow && column % 2 == 1) {
+                        // Adding the negative sign to every second value of the current row
+                        rowValue = rowValue.negate();
+                    }
+                    currentRow.add(rowValue);
+                }
+            }
+            previousRow = currentRow;
+        }
+
+        return currentRow;
+    }
+
+    /**
+     * Complexity O(n^2/2) + O(n), which for large inputs can be noted as O(n^2)
+     *
+     * @param testValue - integer we are testing to be prime or not
+     * @return true if integer is prime
+     */
+    static Boolean isPrimeByAKS(Integer testValue) {
+        if (testValue == null || testValue < 0) {
+            System.out.println("Only natural numbers are allowed as input");
+            return null;
+        }
+
+        // coefficients for (x-1)^n
+        List<BigInteger> coefficients = findCoefficients(testValue);
+        int coefficientListSize = coefficients.size();
+
+        // Loop over the coefficients and check one-by-one if they are divisible by the input number
+        // while adjusting coefficients for (x-1)^n - (x^n-1) = (x-1)^n - x^n + 1
+        // this means skipping the first value and adding 1 to the last value
+        for (int i = 1; i < coefficientListSize; i++) {
+            BigInteger coefficient = coefficients.get(i);
+            // add 1 to the last value
+            if (i == coefficientListSize - 1) {
+                coefficient = coefficient.add(BigInteger.ONE);
+            }
+            if (coefficient.remainder(BigInteger.valueOf(testValue)).compareTo(BigInteger.ZERO) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        // Input value to test here
+        Boolean result = isPrimeByAKS(13129);
+        if (result == null) {
+            System.out.println("Unexpected input");
+        } else if (result) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
+    // 33.3sec for 13127
+    // 7min 31.8sec for 33469
+}
--- src/test/java/itc8190/TestAKSSlow.java
+++ src/test/java/itc8190/TestAKSSlow.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestAKS {
+public class TestAKSSlow {
 
     @Test
     void testCoefficientFinding_4() {
@@ -17,7 +17,7 @@ public class TestAKS {
                 BigInteger.valueOf(1L), BigInteger.valueOf(-4L), BigInteger.valueOf(6L), 
                 BigInteger.valueOf(-4L), BigInteger.valueOf(1L)
         );
-        List<BigInteger> result = AKS.findCoefficients(4);
+        List<BigInteger> result = AKSSlow.findCoefficients(4);
         for (int i = 0; i < expectedResult.size(); i++) {
             assertThat(result.get(i)).isEqualByComparingTo(expectedResult.get(i));
         }
@@ -29,7 +29,7 @@ public class TestAKS {
                 BigInteger.valueOf(1L), BigInteger.valueOf(-7L), BigInteger.valueOf(21L), BigInteger.valueOf(-35L), 
                 BigInteger.valueOf(35L), BigInteger.valueOf(-21L), BigInteger.valueOf(7L), BigInteger.valueOf(-1L)
         );
-        List<BigInteger> result = AKS.findCoefficients(7);
+        List<BigInteger> result = AKSSlow.findCoefficients(7);
         for (int i = 0; i < expectedResult.size(); i++) {
             assertThat(result.get(i)).isEqualByComparingTo(expectedResult.get(i));
         }
@@ -44,7 +44,7 @@ public class TestAKS {
                 BigInteger.valueOf(-2002L), BigInteger.valueOf(1001L), BigInteger.valueOf(-364L), 
                 BigInteger.valueOf(91L), BigInteger.valueOf(-14L), BigInteger.valueOf(1L)
         );
-        List<BigInteger> result = AKS.findCoefficients(14);
+        List<BigInteger> result = AKSSlow.findCoefficients(14);
         for (int i = 0; i < expectedResult.size(); i++) {
             assertThat(result.get(i)).isEqualByComparingTo(expectedResult.get(i));
         }
@@ -54,13 +54,13 @@ public class TestAKS {
     @ParameterizedTest
     @ValueSource(ints = {4, 6, 9, 21, 169, 1207})
     void testIsPrimeByAKS_notPrimeNumbers(int number) {
-        assertThat(AKS.isPrimeByAKS(number)).isFalse();
+        assertThat(AKSSlow.isPrimeByAKS(number)).isFalse();
     }
 
     // prime numbers according to http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 73, 127, 233, 283, 467, 661, 877, 1087, 1223})
     void testIsPrimeByAKS_withPrimeNumbers(int number) {
-        assertThat(AKS.isPrimeByAKS(number)).isTrue();
+        assertThat(AKSSlow.isPrimeByAKS(number)).isTrue();
     }
 }
